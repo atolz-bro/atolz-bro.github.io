287d2c8924db077c1b9ba695599256bc8e948b73
atolz-bro <62163687+atolz-bro@users.noreply.github.com>
2022-08-06 13:54:24 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/img.webp');
+    } else {
+      myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
+
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+}
+
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+
+
